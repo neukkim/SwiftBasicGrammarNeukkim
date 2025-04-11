92b6c1b1a805ddfc4cebf94e0b7c89b38216e374
sunminhong <akoda5555@gmail.com>
2025-04-11 16:59:10 +0900

    14_값_타입과_참조_타입

---
+++ SwiftBasicGrammarNeukkim08.playground/Contents.swift
@@ -0,0 +1,31 @@
+// 14_값_타입과_참조_타입
+// value type, reference type
+
+
+/*
+                Class       Struct      Enum
+ 
+ Type           Reference   Value       Value
+ 
+ SubClassing    O           X           X
+ (상속)
+ 
+ Extention      O           O           O
+ 
+ */
+
+/*
+ 
+ 구조체 언제 사용?:
+ - 참조가 아닌 복사
+ - 상속받을 필요가 없을때
+ 
+ Value
+ - 데이터 전달할 때 값 복사 전달
+ 
+ Reference
+ - 데이터를 전달할 때 값의 메모리 위치를 전달
+ 
+ */
+
+
